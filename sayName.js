f8fb255c75adc5a7b7b17486950822d2ebae0521
Kyle Evans <kylelarryevans@gmail.com>
2016-09-08 20:54:33 -0600

    Completed all exercises

--- sayName.js
+++ sayName.js
@@ -1,13 +1,20 @@
 //Create a Person constructor that accepts name and age as parameters and sets those properties accordingly in the Constructor.
 
-  //code here
+  function Person(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 
 
 //Now create three instances of Person with data you make up
 
-  //code here
+  var bob = new Person("Bob", 60);
+  var kelli = new Person("Kelli", 30);
+  var kyle = new Person("Kyle", 31);
 
 
 //Now add a sayName method on your Person class that will alert the name of whatever Person instance called it.
 
-  //code here
+  Person.prototype.sayName = function() {
+    alert(this.name);
+  }
